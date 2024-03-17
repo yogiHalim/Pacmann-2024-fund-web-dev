05452ad2df3389de7d5e4703ee5136ce294d4e31
yogiHalim <158463832+yogiHalim@users.noreply.github.com>
2024-03-17 19:22:12 +0000

    tambah contoh.html di root dan dalam fodler app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 vApp= Flask(__name__)
 @vApp.route("/")
+console.log(vApp.root_path)
 
-def hello():
-    return "Hello, world. Again"
+def hello():   
+    return render_template("contoh.html")
+    return "Hello, world. Aaaagain"
+
+# if __name__ == __main__:
+#     vApp.run(host='0.0.0.0')
